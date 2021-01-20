049cdbcebc841e1bf7a6fa05886a4b6d902e1212
童德勇 <deyong_tong@foxlinkimage.com.cn>
2021-01-20 17:29:58 +0800

    添加左侧栏

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,71 +1,19 @@
 <template>
-  <div>
-    <div class="leftSideBar">
-      <div class="logoBar">
-        <span>自定义颜色</span>
-      </div>
-      <el-menu
-        :unique-opened="true"
-        default-active="2"
-        class="el-menu-vertical-demo"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b"
-        @open="handleOpen"
-        @close="handleClose"
-      >
-        <el-submenu index="1">
-          <template #title>
-            <i class="el-icon-location" />
-            <span>导航一</span>
-          </template>
-          <el-menu-item-group>
-            <template #title>分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template #title>选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-menu-item index="2">
-          <i class="el-icon-menu" />
-          <template #title>导航二</template>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <i class="el-icon-document" />
-          <template #title>导航三</template>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting" />
-          <template #title>导航四</template>
-        </el-menu-item>
-        <el-submenu index="5">
-          <template #title>
-            <i class="el-icon-location" />
-            <span>导航一</span>
-          </template>
-          <el-menu-item-group>
-            <template #title>分组一</template>
-            <el-menu-item index="5-1">选项1</el-menu-item>
-            <el-menu-item index="5-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="5-3">选项3</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-      </el-menu>
-    </div>
+  <div class="leftSideBar">
+    <LogoSideBar />
+    <SideBarItem />
   </div>
 </template>
 
 <script>
+import LogoSideBar from '@/layout/logoSideBar'
+import SideBarItem from '@/layout/sideBarItem'
 export default {
   name: 'Layout',
+  components: {
+    LogoSideBar,
+    SideBarItem
+  },
   methods: {
     handleOpen(key, keyPath) {
       console.log(key, keyPath)
@@ -79,11 +27,16 @@ export default {
 
 <style scoped>
  .leftSideBar {
-   width: 210px;
+   transition: width 0.28s;
+   width: 200px !important;
+   background-color: #304156;
    height: 100%;
-   background-color: #3a8ee6;
- }
- .logoBar {
-   background-color: ;
+   position: fixed;
+   font-size: 0;
+   top: 0;
+   bottom: 0;
+   left: 0;
+   z-index: 1001;
+   overflow: hidden;
  }
 </style>
---
+++ src/layout/sideBarItem.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <el-menu
+      default-active="1-4-1"
+      class="el-menu-vertical-demo"
+      :collapse="isCollapse"
+      background-color="#304156"
+      text-color="rgb(191, 203, 217)"
+      active-text-color="#ffd04b"
+      @open="handleOpen"
+      @close="handleClose"
+    >
+      <el-submenu index="1">
+        <template #title>
+          <i class="el-icon-location" />
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <template #title>分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template #title>选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu" />
+        <template #title>导航二</template>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document" />
+        <template #title>导航三</template>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting" />
+        <template #title>导航四</template>
+      </el-menu-item>
+    </el-menu>
+    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+      <el-radio-button :label="false">展开</el-radio-button>
+      <el-radio-button :label="true">收起</el-radio-button>
+    </el-radio-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideBarItem',
+  data() {
+    return {
+      isCollapse: true
+    }
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
